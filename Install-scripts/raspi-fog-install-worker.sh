b37f579189a4e098d7e94edccadc7eb0de627b94
shreshthtuli <34661516+shreshthtuli@users.noreply.github.com>
2018-06-19 16:59:36 +1000

    added bash scripts for installation

---
+++ Install-scripts/raspi-fog-install-worker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+apt-get update
+apt-get upgrade -y
+apt-get install oracle-java8-jdk -y
+apt-get install ant git vim -y
+echo "ServerName 127.0.0.1" >> /etc/apache2/apache2.conf
+apt-get install apache2 -y
+apache2ctl configtest
+apt-get install php libapache2-mod-php php-mcrypt php-mysql -y
+service apache2 restart
+apt-get install mysql-server -y
+mysql -u root -praspberry -e "CREATE DATABASE data;GRANT ALL PRIVILEGES ON data.* TO 'root'@'localhost' IDENTIFIED BY 'raspberry';FLUSH PRIVILEGES;"
+echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
+service apache2 restart
+sudo mkdir /var/www/html/HealthKeeper/
+sudo chmod -R 777 /var/www/html/HealthKeeper/
+sudo cp ../Browser-src/Worker/* /var/www/html/HealthKeeper/
+sudo chmod 777 /var/www/html/HeathKeeper/*
+cd /var/www/html/HealthKeeper/
+javac ./analyzer.java
+echo "Successfully Installed Raspi-Fog"
+echo "Note the worker IP address :"
+hostname -I
+echo "Press Enter to run"
+read
+java analyzer
+
